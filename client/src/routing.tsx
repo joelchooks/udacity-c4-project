3fc78ad85bbcbd61e496aea93ed64f865d6e5014
Chooks <joelchukks@gmail.com>
2023-03-09 00:45:33 +0100

    fix type errors

--- client/src/routing.tsx
+++ client/src/routing.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Auth from './auth/Auth'
 import { Router, Route } from 'react-router-dom'
+import Auth from './auth/Auth'
 import Callback from './components/Callback'
-import createHistory from 'history/createBrowserHistory'
 import App from './App';
-const history = createHistory()
+const { createBrowserHistory } = require('history');
+const history = createBrowserHistory()
 
 const auth = new Auth(history)
+
 
 const handleAuthentication = (props: any) => {
   const location = props.location
